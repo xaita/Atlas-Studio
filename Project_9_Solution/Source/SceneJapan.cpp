e08902fd74198e4b05627626f08d5e29bd9bc91c
GHOSTVCL <joanetgiolet@gmail.com>
2022-04-27 18:59:40 +0200

    a

--- Project_9_Solution/Source/SceneJapan.cpp
+++ Project_9_Solution/Source/SceneJapan.cpp
@@ -24,7 +24,7 @@ bool SceneJapan::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("Assets/UI/Select Screens/Map Screen.png");
+	bgTexture = App->textures->Load("Assets/UI/Screens/Japan vs Japan.png");
 	/*select = App->textures->Load("Assets/UI/Select Screens/Purple rectangle.png");*/
 
 	App->audio->PlayMusic("Assets/Audios/Music/01_Get Ready (Select Screen).ogg", 1.0f);
