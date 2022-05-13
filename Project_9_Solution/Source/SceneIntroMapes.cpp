623a58e9a041b1e4e9a1004c1665421b54b5621c
DonnoNonno <paugarrigab@gmail.com>
2022-05-13 09:15:55 +0200

    Seleccion de mapas V1

--- Project_9_Solution/Source/SceneIntroMapes.cpp
+++ Project_9_Solution/Source/SceneIntroMapes.cpp
@@ -25,9 +25,17 @@ bool SceneIntroMapes::Start()
 	bool ret = true;
 
 	bgTexture = App->textures->Load("Assets/UI/Select Screens/Map Screen.png");
-	/*select = App->textures->Load("Assets/UI/Select Screens/Purple rectangle.png");*/
+	
+	select = App->textures->Load("Assets/UI/Select Screens/Purple rectangle.png");
 
 	
+	X1 = 164;
+	X2 = 164;
+	Y1 = 56;
+	Y2 = 56;
+	Ready1 = false;
+	Ready2 = false;
+
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
@@ -37,7 +45,43 @@ bool SceneIntroMapes::Start()
 
 Update_Status SceneIntroMapes::Update()
 {
-	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
+	//player 1
+	if (App->input->keys[SDL_SCANCODE_S] == Key_State::KEY_DOWN && X1 != 150 && Ready1 == false) {
+		Y1 += 50;
+	}
+	if (App->input->keys[SDL_SCANCODE_W] == Key_State::KEY_DOWN && X1 != 50 && Ready1 == false) {
+		Y1 -= 50;
+	}
+	if (App->input->keys[SDL_SCANCODE_D] == Key_State::KEY_DOWN && X1 != 100 && Ready1 == false) {
+		X1 += 50;
+	}
+	if (App->input->keys[SDL_SCANCODE_A] == Key_State::KEY_DOWN && X1 != 50 && Ready1 == false) {
+		X1 -= 50;
+	}
+
+	//Player 2
+	if (App->input->keys[SDL_SCANCODE_DOWN] == Key_State::KEY_DOWN && X1 != 150 && Ready2 == false) {
+		Y1 += 50;
+	}
+	if (App->input->keys[SDL_SCANCODE_UP] == Key_State::KEY_DOWN && X1 != 50 && Ready2 == false) {
+		Y1 -= 50;
+	}
+	if (App->input->keys[SDL_SCANCODE_RIGHT] == Key_State::KEY_DOWN && X1 != 100 && Ready2 == false) {
+		X1 += 50;
+	}
+	if (App->input->keys[SDL_SCANCODE_LEFT] == Key_State::KEY_DOWN && X1 != 50 && Ready2 == false) {
+		X1 -= 50;
+	}
+	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN) {
+		Ready1 = true;
+	}
+	if (App->input->keys[SDL_SCANCODE_O] == Key_State::KEY_DOWN) {
+		Ready2 = true;
+	}
+	
+	
+	
+	if (Ready1 == true && Ready2 == true)
 	{
 		App->fade->FadeToBlack(this, (Module*)App->sceneJapan, 90);
 	}
@@ -51,7 +95,7 @@ Update_Status SceneIntroMapes::PostUpdate()
 {
 	// Draw everything --------------------------------------
 	App->render->Blit(bgTexture, 0, 0, NULL);
-	App->render->Blit(select, 164, 56, NULL); //rectangle de seleccionar stage
+	App->render->Blit(select, X1, Y1, NULL); //rectangle de seleccionar stage
 
 	return Update_Status::UPDATE_CONTINUE;
 }
--- Project_9_Solution/Source/SceneIntroMapes.h
+++ Project_9_Solution/Source/SceneIntroMapes.h
@@ -31,6 +31,13 @@ public:
 	// The scene sprite sheet loaded into an SDL_Texture
 	SDL_Texture* bgTexture = nullptr;
 	SDL_Texture* select = nullptr;
+
+	int X1;
+	int Y1;
+	int X2;
+	int Y2;
+	bool Ready1;
+	bool Ready2;
 };
 
 #endif
