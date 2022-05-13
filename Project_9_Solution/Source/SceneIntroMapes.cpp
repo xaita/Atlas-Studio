a55493bc1e0c262020db69aa2e7052251f241237
DonnoNonno <paugarrigab@gmail.com>
2022-05-13 09:31:59 +0200

    Update SceneIntroMapes.cpp

--- Project_9_Solution/Source/SceneIntroMapes.cpp
+++ Project_9_Solution/Source/SceneIntroMapes.cpp
@@ -46,42 +46,32 @@ bool SceneIntroMapes::Start()
 Update_Status SceneIntroMapes::Update()
 {
 	//player 1
-	if (App->input->keys[SDL_SCANCODE_S] == Key_State::KEY_DOWN && X1 != 150 && Ready1 == false) {
-		Y1 += 50;
+	if (App->input->keys[SDL_SCANCODE_S] == Key_State::KEY_DOWN && Y1 != 56 + 46 && Ready1 == false) {
+		Y1 += 23;
 	}
-	if (App->input->keys[SDL_SCANCODE_W] == Key_State::KEY_DOWN && X1 != 50 && Ready1 == false) {
-		Y1 -= 50;
+	if (App->input->keys[SDL_SCANCODE_W] == Key_State::KEY_DOWN && Y1 != 56 && Ready1 == false) {
+		Y1 -= 23;
 	}
-	if (App->input->keys[SDL_SCANCODE_D] == Key_State::KEY_DOWN && X1 != 100 && Ready1 == false) {
-		X1 += 50;
-	}
-	if (App->input->keys[SDL_SCANCODE_A] == Key_State::KEY_DOWN && X1 != 50 && Ready1 == false) {
-		X1 -= 50;
-	}
+	
 
 	//Player 2
-	if (App->input->keys[SDL_SCANCODE_DOWN] == Key_State::KEY_DOWN && X1 != 150 && Ready2 == false) {
-		Y1 += 50;
+	/*if (App->input->keys[SDL_SCANCODE_DOWN] == Key_State::KEY_DOWN && Y2 != 150 && Ready2 == false) {
+		Y2 += 50;
 	}
-	if (App->input->keys[SDL_SCANCODE_UP] == Key_State::KEY_DOWN && X1 != 50 && Ready2 == false) {
-		Y1 -= 50;
-	}
-	if (App->input->keys[SDL_SCANCODE_RIGHT] == Key_State::KEY_DOWN && X1 != 100 && Ready2 == false) {
-		X1 += 50;
-	}
-	if (App->input->keys[SDL_SCANCODE_LEFT] == Key_State::KEY_DOWN && X1 != 50 && Ready2 == false) {
-		X1 -= 50;
-	}
+	if (App->input->keys[SDL_SCANCODE_UP] == Key_State::KEY_DOWN && Y2 != 50 && Ready2 == false) {
+		Y2 -= 50;
+	}*/
+	
 	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN) {
 		Ready1 = true;
 	}
-	if (App->input->keys[SDL_SCANCODE_O] == Key_State::KEY_DOWN) {
+	/*if (App->input->keys[SDL_SCANCODE_O] == Key_State::KEY_DOWN) {
 		Ready2 = true;
-	}
+	}*/
 	
 	
 	
-	if (Ready1 == true && Ready2 == true)
+	if (Ready1 == true /*&& Ready2 == true*/)
 	{
 		App->fade->FadeToBlack(this, (Module*)App->sceneJapan, 90);
 	}
@@ -96,6 +86,6 @@ Update_Status SceneIntroMapes::PostUpdate()
 	// Draw everything --------------------------------------
 	App->render->Blit(bgTexture, 0, 0, NULL);
 	App->render->Blit(select, X1, Y1, NULL); //rectangle de seleccionar stage
-
+	
 	return Update_Status::UPDATE_CONTINUE;
 }
